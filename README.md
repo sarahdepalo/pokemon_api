2cde97ac05cd7c2b327642aa70f384e21db28009
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-29 12:55:27 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pokemon API - Original 151 Pokemon
+
+This is a small webpage that displays all of the original 151 pokemon fetched from the [PokeApi](https://pokeapi.co/). JavaScript was used to fetch each Pokemon's name, type, and sprite. 
+
+![image of site](pokeScreenshot.png)
---
+++ js/scripts.js
@@ -0,0 +1,60 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    function fetchPokemon() {
+        fetch(`https://pokeapi.co/api/v2/pokemon?limit=151%27`).then(function(response) {
+            return response.json();
+        }).then(function(allPokemon) {
+            allPokemon.results.forEach(function (pokemon) {
+                fetchPokemonData(pokemon);
+                
+            })
+        })
+    }
+
+    function fetchPokemonData(pokemon) {
+        let url = pokemon.url;
+        fetch(url).then(function(response) {
+            return response.json();
+        }).then(function(pokeData) {
+            generatePokemon(pokeData);
+            console.log(pokeData);
+        })
+    }
+
+    function generatePokemon(pokeData) {
+
+        //Gets Pokemon Name
+        const pokeName = document.createElement('h3');
+        pokeName.innerText = pokeData.name.toUpperCase();
+
+        //Gets Sprite Image
+        const spriteImage = document.createElement('img');
+        const spriteImageSrc = pokeData.sprites.front_default;
+        spriteImage.src = spriteImageSrc;
+
+        //Gets Types
+        const pokeTypes = document.createElement('ul');
+        fetchTypes(pokeData.types, pokeTypes); //uses a function to append the type to an ul
+
+        const pokeContainer = document.querySelector('#pokeContainer');
+        const individualContainers = document.createElement('div');
+        individualContainers.classList.add('pokeContainers')
+
+        individualContainers.append(pokeName, spriteImage, pokeTypes)
+        pokeContainer.appendChild(individualContainers);
+    }
+
+    function fetchTypes(types, ul) {
+        types.forEach(function(type) {
+            let typeLiEl = document.createElement('li');
+            typeLiEl.innerText = type['type']['name'];
+            ul.append(typeLiEl);
+        })
+    }
+
+    fetchPokemon();
+})
+
+
